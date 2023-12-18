e438597fbbf95d3986046b7841f83d198457031c
phuc98it <phucpham98.it.vn@gmail.com>
2023-12-18 21:48:10 +0700

    event-buffer-stream

--- README.md
+++ README.md
@@ -31,3 +31,52 @@
 
 - npm start -- touch ./file.txt
 - npm start -- cat ./file.txt 
+
+# EventEmitter
+- method:
+    + on:  nhận info sự kiện và khai báo sẵn cách xử  lí sự kiện đó.
+    + emit: phát sự kiện kèm info.
+
+eventEmitter.on("event", function (data) {      // declare handle evnet
+  console.log(data); // { key: value }
+  console.log(this === eventEmitter); // true
+});
+
+eventEmitter.emit("event", {                    // phat event
+  key: "value",
+});
+
+eventEmitter.removeListener('event', listener); // xóa event
+    
+# Buffer
+    - Dữ liệu dạng nhị phân.
+    - Để biểu diễn những dữ liệu phức tạp như: hình ảnh, video, ...
+    - Buffer is an array of numbers. Và nó là 1 mang byte -> mỗi thành phần là 1 số  có giá trị từ 0 -> 255.
+    - Không thể  thay đổi kích thước khi Buffer được tạo ra.
+
+    .alloc(size, fill)  -> tạo Buffer với khởi tạo số  lượng thành phần (size) ban đầu trong mảng.
+    .from('<string>')   -> .from Buffer chứa dữ liệu truyền vào
+
+# Stream
+    - Ban đầu chưa có Stream, khi đọc file hệ thống phải chờ đợi toàn bộ dữ liệu trong tệp tải vào bộ nhớ trước khi thực hiện bất kỳ hành động nào. => dẫn đến có thể quá tải tài nguyên hệ thống trong 1 thời điểm nào đó.
+    - "Every stream is an instance of EventEmitter" => chúng ta có thể  lắng nghe mọi dữ liệu đến = cách sử dụng EventEmitter API. 
+    - Stream sẽ lắng nghe sự kiện 'data' event ở mọi thời điểm mà 'stream emit' phát ra kèm trong dữ liệu 'chuck'.
+    - Every chucks là 1 instance của Buffer (dữ liệu kiểu mảng, chứa các giá trị nhị phân 0->255, mỗi thành phần tương ứng với 1 byte)
+        => Từ dữ liệu ban đầu trong file, được chuyển hóa về dang buffer mảng -> dẫn đến việc có thể  chuyển hóa dữ liệu ban đầu thành những miếng nhỏ -> memory ko bị quá tải trong 1 thời điểm nhất định.
+    - readable stream có 2 chế  độ:  paused và flowing.
+        + b1: mặc định stream ở chế  độ 'paused'.
+        + b2: khi lắng nghe bắt đc sự kiện 'data' event -> thì chuyển sang chế độ 'flowing' đính kèm dữ liệu đến.  
+
+    - writeable stream : ghi từng phần dữ liệu vào file.
+
+    - Pipes : đẩy từng phần dữ liệu khi thực hiện readableStream tới writeableStream để ghi vào file 1 cách nhanh chóng và đảm bảo, theo 1 con đường khác.
+        + Khi tất cả dữ liệu được hoàn tất, readableStream sẽ phát 'end' event, và writeableStream kết thúc với writable.end function.
+
+    ** Lưu ý : Nếu xảy ra lỗi trong quá trình xử  lí, thì nội dung có thể  không tự động 'closed', do đó cần phải theo dõi và đóng 1 cách thủ công.
+
+    ** Chung quy có thể  có nhiều cách viết để  thực hiện đọc ghi file trong Nodejs, thực chất là dùng callback, stream, buffer.
+        +) 1 số lib giúp biến thể  cách viết theo Promise hoặc async-await.
+
+
+
+
--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
+import * as fs from 'fs';
+import * as EventEmitter from "events";
 const {add, subtract} = require('./utilities')
 import touch from './utils/touch'
 import cat from './utils/cat'
+import { StringDecoder } from "string_decoder";
 
 console.log(add(1,2))
 console.log(subtract(9,5))
@@ -31,3 +34,160 @@ if (command && path) {
 } else {
   console.log('Command missing');
 }
+
+// ==== Event Emitter ==========================
+
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('event', function() {
+  console.log('one');
+});
+eventEmitter.on('event', function() {
+  console.log('two');
+});
+eventEmitter.on('event', function() {
+  console.log('three');
+});
+
+eventEmitter.emit("event");
+
+eventEmitter.on("event", function () {
+  console.log("Event occured!"); // not logged into the console
+});
+
+// Passing additional data to listeners
+
+eventEmitter.on("event1", function (data) {
+  console.log(data); // { key: value }
+  console.log(this === eventEmitter); // true
+});
+
+eventEmitter.on('event2', () => {
+    console.log(this === eventEmitter); // false
+});
+
+eventEmitter.emit("event1", {
+  key: "value",
+});
+
+eventEmitter.emit("event2");
+
+// Remove listeners
+
+function listener () {
+  console.log('Event occurred 2!');
+}
+ 
+eventEmitter.on('event', listener);
+eventEmitter.emit('event'); // Event occurred 2!
+ 
+eventEmitter.removeListener('event', listener);
+ 
+eventEmitter.emit('event'); /// Nothing happened 2
+ 
+
+// Tránh bị lặp vô hạn để  call các event lẫn nhau: dùng setTimeout
+/**
+eventEmitter.on('event1-1', () => {
+  setTimeout(() => {
+      console.log('First event here!');
+      eventEmitter.emit('event2-1');
+  })
+});
+ 
+eventEmitter.on('event2-1', () => {
+  setTimeout(() => {
+      console.log('Second event here!');
+      eventEmitter.emit('event3-1');
+  })
+});
+ 
+eventEmitter.on('event3-1', () => {
+  setTimeout(() => {
+      console.log('Third event here!');
+      eventEmitter.emit('event1-1');
+  })
+});
+ 
+eventEmitter.emit('event1-1');
+ */
+
+// ===== Buffer ===== [0, 255]
+const buffer = Buffer.alloc(5);
+console.log(buffer)
+ 
+buffer[0] = 255;
+console.log(buffer[0]); // 255
+ 
+buffer[1] = 256;
+console.log(buffer[1]); // 0
+ 
+buffer[2] = 260;
+console.log(buffer[2]); // 4
+console.log(buffer[2] === 260%256); // true
+ 
+buffer[3] = 516;
+console.log(buffer[3]); // 4
+console.log(buffer[3] === 516%256); // true
+ 
+buffer[4] = -50;
+console.log(buffer[4]); // 206
+
+
+// Creates a Buffer containing 1, 2, 3
+const buffer2 = Buffer.from([1, 2, 3]);
+console.log(buffer2[2])
+
+const buffer3 = Buffer.from('Hello world!'); 
+console.log(buffer3.toString()); // Hello world!
+
+const buffers4 = [
+  Buffer.from('Hello2 '),
+  Buffer.from([0b11110000, 0b10011111]),  //data ko hoàn chỉnh
+  Buffer.from([0b10001100, 0b10001110]),  //data ko hoàn chỉnh
+  Buffer.from(' world!'),
+];
+
+let result = '';
+buffers4.forEach((buffer) => {
+  result += buffer.toString();
+});
+console.log(result); // Hello ��� world!
+
+const decoder = new StringDecoder('utf8');
+
+let resultDecoder = '';
+buffers4.forEach((buffer) => {
+  resultDecoder += buffer.toString();
+});
+console.log(resultDecoder); // Hello <hình trái đất> world!
+
+
+// ==== Stream ====
+const streamRead = fs.createReadStream(
+  './file-read.txt',
+  {
+    encoding: 'utf-8'
+  }
+)
+
+setTimeout(() => {
+  streamRead.on('data', (chunk) => {  // đọc data từ file
+    console.log(chunk);               // từng đoạn lấy data sẽ đc lưu vào chuck
+  })
+}, 2000);
+
+const streamWrite = fs.createWriteStream('./file-write.txt');
+
+streamWrite.on('finish', () => {
+  console.log("All the data is transmitted !")
+})
+
+streamWrite.write('Hello')    // ghi data vào file
+streamWrite.write('Hi')
+
+streamRead.on('data', chunk => {    // ghi từng đoạn data vào file
+  streamWrite.write(chunk);
+});
+
+streamRead.pipe(streamWrite)
